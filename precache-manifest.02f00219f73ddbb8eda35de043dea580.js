19fce1bdb799c9a3106d77b6f5b80dee69998f5b
Dan Kov <0x666c@mail.ru>
2021-03-07 00:56:33 +0300

    jiftoo.dev

---
+++ precache-manifest.02f00219f73ddbb8eda35de043dea580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc67ce8a5bc04942b2aa931af8921f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f30548d1de4c173750ff",
+    "url": "/static/css/main.688b65f2.chunk.css"
+  },
+  {
+    "revision": "4cb6d241e26b019a2b0f",
+    "url": "/static/js/2.86d4725c.chunk.js"
+  },
+  {
+    "revision": "c5736e10a027dc7eb2aacc7f7cc474aa",
+    "url": "/static/js/2.86d4725c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30548d1de4c173750ff",
+    "url": "/static/js/main.4bcf40b7.chunk.js"
+  },
+  {
+    "revision": "607453c95d7dc95706f9",
+    "url": "/static/js/runtime-main.d0588bb2.js"
+  }
+]);
